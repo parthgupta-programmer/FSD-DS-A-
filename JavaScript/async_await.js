b42588dcf1e2660ce0709e5579fc35dd4e2f642d
parthgupta-programmer <parthg.info@gmail.com>
2026-09-10 14:36:47 +0530

    Async and Await

---
+++ JavaScript/async_await.js
@@ -0,0 +1,47 @@
+function f1(){
+
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+        console.log('F1 Function.');
+        resolve();
+        },4000);
+    })
+
+};
+
+
+function f2(){
+
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+        console.log('F2 Function.');
+        resolve();
+        },2000);
+    })
+
+};
+
+
+f1().then(f2).catch((err)=>
+    console.log("Error",err)
+);
+
+
+
+async function test(){
+    try{
+        await f1();
+        await f2();
+    }
+    catch (err){
+        console.log('Error',err);
+    }
+
+};
+test();
+
+
+
+
+
+
